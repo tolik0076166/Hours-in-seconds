a58422497f9ea732a1e49307ec89468b556a85d5
tolik00761666 <tolik007616@gmail.com>
2023-03-20 15:37:44 +0200

    hours-in-seconds

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let HOURS = prompt('How many hours?');
 HOURS = Number(HOURS);
 
 //I count how many seconds there are in this number of hours and write the calculated value to a variable
-let SECONDS = HOURS * 60;
+let SECONDS = HOURS * 60 *60;
 
 //Display the results using an alert
 alert(`${HOURS} hours is ${SECONDS} seconds`);
